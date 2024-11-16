3573df2453e24637df8f7071298cf2df190727c9
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2024-11-16 17:37:58 +0000

    Feed. You. Stuff. No time.

--- crates/automapper/src/rodc_util.rs
+++ crates/automapper/src/rodc_util.rs
@@ -1,8 +1,17 @@
 use anyhow::{anyhow, bail, Context};
 use rustdoc_types::{Crate, Item, ItemSummary};
 
-fn find_struct_by_name(name: syn::Path, rdocs: &Crate) -> anyhow::Result<StructWrapper> {
-    let (exact_match, path_entry_id, path_entry) = {
+//
+// Public
+//
+
+/// Find structs by name.
+///
+/// This function will return a list of structs that match the given name partially or exactly.
+/// Check the [StructWrapper::is_exact_match] field to see if the match was exact or not.
+///
+pub fn find_struct_by_name(name: syn::Path, rdocs: &Crate) -> anyhow::Result<Vec<StructWrapper>> {
+    let matching_structs = {
         let segments = name
             .segments
             .iter()
@@ -13,19 +22,38 @@ fn find_struct_by_name(name: syn::Path, rdocs: &Crate) -> anyhow::Result<StructW
             .paths
             .iter()
             .find(|(_, p)| p.path == segments)
-            .map(|(id, m)| (true, id, m))
+            .map(|(id, m)| vec![(true, id, m)])
             .or_else(|| {
-                rdocs
-                    .paths
-                    .iter()
-                    .find(|(_, p)| p.path.ends_with(&segments))
-                    .map(|(id, m)| (false, id, m))
+                Some(
+                    rdocs
+                        .paths
+                        .iter()
+                        .filter(|(_, p)| p.path.ends_with(&segments))
+                        .map(|(id, m)| (false, id, m))
+                        .collect::<Vec<_>>(),
+                )
             })
     }
     .context("find struct by name")?;
 
-    dbg!(&exact_match, &path_entry);
+    matching_structs
+        .iter()
+        .map(|(exact_match, path_entry_id, path_entry)| {
+            _find_struct_with_resolved_fields(path_entry, rdocs, path_entry_id, exact_match)
+        })
+        .collect::<anyhow::Result<Vec<_>>>()
+}
 
+//
+// Private
+//
+
+fn _find_struct_with_resolved_fields(
+    path_entry: &&ItemSummary,
+    rdocs: &Crate,
+    path_entry_id: &&rustdoc_types::Id,
+    exact_match: &bool,
+) -> Result<StructWrapper, anyhow::Error> {
     let rustdoc_types::ItemKind::Struct = path_entry.kind else {
         bail!("not a struct type")
     };
@@ -40,36 +68,9 @@ fn find_struct_by_name(name: syn::Path, rdocs: &Crate) -> anyhow::Result<StructW
         unreachable!("must be a struct type",)
     };
 
-    fn resolve_fields(rdocs: &Crate, fields: &[rustdoc_types::Id]) -> Vec<StructField> {
-        fields
-            .iter()
-            .flat_map(|id| rdocs.index.get(id))
-            .map(|item| {
-                let rustdoc_types::ItemEnum::StructField(ty) = &item.inner else {
-                    unreachable!("must be a struct field")
-                };
-                dbg!(&ty);
-                let kind = match ty {
-                    rustdoc_types::Type::ResolvedPath(path) => {
-                        StructFieldKind::ResolvedPath { path: path.clone() }
-                    }
-                    rustdoc_types::Type::Primitive(ty) => {
-                        StructFieldKind::Primitive { name: ty.clone() }
-                    }
-                    _ => unimplemented!("only struct field with resolved path is supported"),
-                };
-
-                StructField {
-                    name: item.name.clone(),
-                    kind,
-                }
-            })
-            .collect::<Vec<_>>()
-    }
-
     let kind = match &struct_.kind {
         rustdoc_types::StructKind::Unit => StructKind::Unit,
-        rustdoc_types::StructKind::Tuple(vec) => StructKind::Tuple(resolve_fields(
+        rustdoc_types::StructKind::Tuple(vec) => StructKind::Tuple(_resolve_fields(
             rdocs,
             &vec.iter().flatten().map(|c| c.clone()).collect::<Vec<_>>(),
         )),
@@ -77,40 +78,67 @@ fn find_struct_by_name(name: syn::Path, rdocs: &Crate) -> anyhow::Result<StructW
             fields,
             has_stripped_fields: _,
         } => StructKind::Plain {
-            fields: resolve_fields(rdocs, &fields),
+            fields: _resolve_fields(rdocs, &fields),
         },
     };
 
     Ok(StructWrapper {
-        is_exact_match: exact_match,
+        is_exact_match: *exact_match,
         path: path_entry.path.clone(),
         kind,
     })
 }
 
+fn _resolve_fields(rdocs: &Crate, fields: &[rustdoc_types::Id]) -> Vec<StructField> {
+    fields
+        .iter()
+        .flat_map(|id| rdocs.index.get(id))
+        .map(|item| {
+            let rustdoc_types::ItemEnum::StructField(ty) = &item.inner else {
+                unreachable!("must be a struct field")
+            };
+            dbg!(&ty);
+            let kind = match ty {
+                rustdoc_types::Type::ResolvedPath(path) => {
+                    StructFieldKind::ResolvedPath { path: path.clone() }
+                }
+                rustdoc_types::Type::Primitive(ty) => {
+                    StructFieldKind::Primitive { name: ty.clone() }
+                }
+                _ => unimplemented!("only struct kind plain or resolved path are supported"),
+            };
+
+            StructField {
+                name: item.name.clone(),
+                kind,
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
 #[derive(Debug)]
-struct StructWrapper {
+pub struct StructWrapper {
     is_exact_match: bool,
     path: Vec<String>,
     kind: StructKind,
 }
 
 #[derive(Debug)]
-enum StructKind {
+pub enum StructKind {
     Unit,
     Tuple(Vec<StructField>),
     Plain { fields: Vec<StructField> },
 }
 
 #[derive(Debug)]
-struct StructField {
+pub struct StructField {
     /// Unset for tuple fields
     name: Option<String>,
     kind: StructFieldKind,
 }
 
 #[derive(Debug)]
-enum StructFieldKind {
+pub enum StructFieldKind {
     Primitive { name: String },
     ResolvedPath { path: rustdoc_types::Path },
 }
