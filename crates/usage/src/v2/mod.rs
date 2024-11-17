8aa620ab4a671f15c3160753da4f9c6b34ea483e
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2024-11-17 09:49:59 +0000

    Your heart's been aching but you're too shy to say it

--- crates/usage/src/v2/mod.rs
+++ crates/usage/src/v2/mod.rs
@@ -30,3 +30,37 @@ fn basic_struct() {
     assert_eq!(input.b, output.b);
     assert_eq!(input.s, output.s);
 }
+
+#[derive(Debug, Clone)]
+struct SourceStruct2 {
+    s: String,
+    nested: SourceStruct,
+}
+
+#[derive(Debug, Clone)]
+struct DestStruct2 {
+    s: String,
+    nested: DestStruct,
+}
+
+#[test]
+fn nested_struct() {
+    use crate::v2;
+    let input = SourceStruct2 {
+        s: "hello".to_string(),
+        nested: SourceStruct {
+            a: 1,
+            b: 2,
+            s: "world".to_string(),
+        },
+    };
+
+    automapper::map! {
+        fn mapping(v2::SourceStruct2, v2::DestStruct2);
+    };
+    let output = mapping(input.clone());
+    assert_eq!(input.s, output.s);
+    assert_eq!(input.nested.a, output.nested.a);
+    assert_eq!(input.nested.b, output.nested.b);
+    assert_eq!(input.nested.s, output.nested.s);
+}
