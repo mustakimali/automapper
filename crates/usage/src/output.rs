8dcbe42e2895f2fbce5849620532ec94b2bfd3b4
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2024-12-03 21:09:08 +0000

    Ok

--- crates/usage/src/output.rs
+++ crates/usage/src/output.rs
@@ -175,3 +175,179 @@ DestStruct4
     }
 }
         }
+
+        pub mod mod_1733260126224_mwab29f82vmprn {
+            fn __map(value : crate :: models :: SourceStruct) -> crate :: models ::
+DestStruct
+{ crate :: models :: DestStruct { a : value.a, b : value.b, s : value.s, } }
+        }
+
+        pub mod mod_1733260126225_htffbz8c7ehca9 {
+            fn __map(value : crate :: models :: SourceStruct) -> crate :: models ::
+DestStruct
+{ crate :: models :: DestStruct { a : value.a, b : value.b, s : value.s, } }
+        }
+
+        pub mod mod_1733260126291_rk5sczjw65fkkd {
+            fn __map(value : crate :: models :: SourceStruct2) -> crate :: models ::
+DestStruct2
+{
+    crate :: models :: DestStruct2
+    {
+        s : value.s, nested : crate :: models :: DestStruct
+        { a : value.nested.a, b : value.nested.b, s : value.nested.s, },
+    }
+}
+        }
+
+        pub mod mod_1733260126294_rk6gz5qj20bz0m {
+            fn __map(value : crate :: models :: SourceStruct2) -> crate :: models ::
+DestStruct2
+{
+    crate :: models :: DestStruct2
+    {
+        s : value.s, nested : crate :: models :: DestStruct
+        { a : value.nested.a, b : value.nested.b, s : value.nested.s, },
+    }
+}
+        }
+
+        pub mod mod_1733260126357_0rgchdxw9c5kgx {
+            fn __map(value : crate :: models :: SourceStruct3) -> crate :: models ::
+DestStruct4
+{
+    crate :: models :: DestStruct4
+    {
+        s : value.s, nested : crate :: models :: DestStruct
+        { a : value.nested.a, b : value.nested.b, s : value.nested.s, },
+        optional :
+        value.optional.map(| v |
+        { crate :: models :: DestStruct { a : v.a, b : v.b, s : v.s, } }),
+    }
+}
+        }
+
+        pub mod mod_1733260126371_3r2agf1av3pqve {
+            fn __map(value : crate :: models :: SourceStruct3) -> crate :: models ::
+DestStruct4
+{
+    crate :: models :: DestStruct4
+    {
+        s : value.s, nested : crate :: models :: DestStruct
+        { a : value.nested.a, b : value.nested.b, s : value.nested.s, },
+        optional :
+        value.optional.map(| v |
+        { crate :: models :: DestStruct { a : v.a, b : v.b, s : v.s, } }),
+    }
+}
+        }
+
+        pub mod mod_1733260126425_t6jt2bty7k9w11 {
+            fn __map(value : crate :: models :: SourceStructWithEnum) -> crate :: models
+:: DestStructWithEnum
+{
+    crate :: models :: DestStructWithEnum
+    {
+        enum_ : match value.enum_
+        {
+            crate :: models :: SourceEnumBasic :: Unit => crate :: models ::
+            DestEnumBasic :: Unit, crate :: models :: SourceEnumBasic ::
+            Touple(item_0, item_1) => crate :: models :: DestEnumBasic ::
+            Touple(item_0, item_1), crate :: models :: SourceEnumBasic ::
+            ToupleSingle(item_0) => crate :: models :: DestEnumBasic ::
+            ToupleSingle(item_0), crate :: models :: SourceEnumBasic :: Struct
+            { field1, field2, nested } => crate :: models :: DestEnumBasic ::
+            Struct
+            {
+                field1 : field1, field2 : field2, nested : crate :: models ::
+                DestStruct { a : nested.a, b : nested.b, s : nested.s, },
+            },
+        }, field : value.field,
+    }
+}
+        }
+
+        pub mod mod_1733260126443_6tjqpyt2cz3yjn {
+            fn __map(value : crate :: models :: SourceStructWithEnum) -> crate :: models
+:: DestStructWithEnum
+{
+    crate :: models :: DestStructWithEnum
+    {
+        enum_ : match value.enum_
+        {
+            crate :: models :: SourceEnumBasic :: Unit => crate :: models ::
+            DestEnumBasic :: Unit, crate :: models :: SourceEnumBasic ::
+            Touple(item_0, item_1) => crate :: models :: DestEnumBasic ::
+            Touple(item_0, item_1), crate :: models :: SourceEnumBasic ::
+            ToupleSingle(item_0) => crate :: models :: DestEnumBasic ::
+            ToupleSingle(item_0), crate :: models :: SourceEnumBasic :: Struct
+            { field1, field2, nested } => crate :: models :: DestEnumBasic ::
+            Struct
+            {
+                field1 : field1, field2 : field2, nested : crate :: models ::
+                DestStruct { a : nested.a, b : nested.b, s : nested.s, },
+            },
+        }, field : value.field,
+    }
+}
+        }
+
+        pub mod mod_1733260126496_r5qepcvtz86zsg {
+            fn map_proto_struct(value : crate :: protogen :: example :: Person) -> crate
+:: protogen :: example :: HomoSepiens
+{
+    crate :: protogen :: example :: HomoSepiens
+    {
+        first_name : value.first_name, last_name : value.last_name, gender :
+        value.gender.map(| v |
+        {
+            crate :: protogen :: example :: HomoSepiensGender
+            {
+                gender :
+                v.gender.map(| v |
+                {
+                    match v
+                    {
+                        crate :: protogen :: example :: gender :: Gender ::
+                        Male(item_0) => crate :: protogen :: example ::
+                        homo_sepiens_gender :: Gender :: Male(item_0), crate ::
+                        protogen :: example :: gender :: Gender :: Female(item_0) =>
+                        crate :: protogen :: example :: homo_sepiens_gender ::
+                        Gender :: Female(item_0),
+                    }
+                }),
+            }
+        }),
+    }
+}
+        }
+
+        pub mod mod_1733260126515_d830jc6k7prfzg {
+            fn map_proto_struct(value : crate :: protogen :: example :: Person) -> crate
+:: protogen :: example :: HomoSepiens
+{
+    crate :: protogen :: example :: HomoSepiens
+    {
+        first_name : value.first_name, last_name : value.last_name, gender :
+        value.gender.map(| v |
+        {
+            crate :: protogen :: example :: HomoSepiensGender
+            {
+                gender :
+                v.gender.map(| v |
+                {
+                    match v
+                    {
+                        crate :: protogen :: example :: gender :: Gender ::
+                        Male(item_0) => crate :: protogen :: example ::
+                        homo_sepiens_gender :: Gender :: Male(item_0), crate ::
+                        protogen :: example :: gender :: Gender :: Female(item_0) =>
+                        crate :: protogen :: example :: homo_sepiens_gender ::
+                        Gender :: Female(item_0),
+                    }
+                }),
+            }
+        }),
+    }
+}
+        }
